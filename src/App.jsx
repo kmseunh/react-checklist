0183c35d4c689bc437d0ffe821ccd710cfb7208e
kmseunh <tmdgus8779@gmail.com>
2024-01-17 16:52:13 +0900

    refactor: Styled Components를 사용하여 UI 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import styled from 'styled-components';
 import CheckList from './CheckList';
 import { CheckListProvider } from './context/CheckListContext';
 
+const AppWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 20px;
+`;
+
 export function App() {
     return (
-        <CheckListProvider>
-            <CheckList />
-        </CheckListProvider>
+        <AppWrapper>
+            <CheckListProvider>
+                <CheckList />
+            </CheckListProvider>
+        </AppWrapper>
     );
 }
 
--- src/CheckList.jsx
+++ src/CheckList.jsx
@@ -1,4 +1,34 @@
+import styled from 'styled-components';
 import { useCheckList } from './context/CheckListContext';
+
+const CheckListWrapper = styled.div`
+    text-align: left;
+    width: 300px; /* Adjust the width according to your design */
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+const ChecklistTitle = styled.h2`
+    font-size: 20px;
+    margin-bottom: 20px;
+`;
+
+const ItemWrapper = styled.li`
+    margin-bottom: 10px;
+    list-style: none;
+`;
+
+const CheckboxLabel = styled.label`
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+`;
+
+const CheckboxInput = styled.input`
+    margin-right: 10px;
+`;
 
 export default function CheckList() {
     const { items, initialCheckList } = useCheckList();
@@ -6,14 +36,14 @@ export default function CheckList() {
     const root = items[0];
     const itemIds = root.childItems;
     return (
-        <>
-            <h2>체크리스트</h2>
+        <CheckListWrapper>
+            <ChecklistTitle>체크리스트</ChecklistTitle>
             <ol>
                 {itemIds.map((id) => (
                     <ItemTree key={id} id={id} parentId={0} />
                 ))}
             </ol>
-        </>
+        </CheckListWrapper>
     );
 }
 
@@ -23,9 +53,15 @@ function ItemTree({ id, parentId }) {
     const childItems = item.childItems;
 
     return (
-        <li>
-            {item.title} &nbsp;
-            <button onClick={() => handleComplete(parentId, id)}>완료</button>
+        <ItemWrapper>
+            <CheckboxLabel>
+                <CheckboxInput
+                    type='checkbox'
+                    checked={childItems} // Check if there are no child items
+                    onChange={() => handleComplete(parentId, id)}
+                />
+                {item.title}
+            </CheckboxLabel>
             {childItems.length > 0 && (
                 <ol>
                     {childItems.map((childId) => (
@@ -33,6 +69,6 @@ function ItemTree({ id, parentId }) {
                     ))}
                 </ol>
             )}
-        </li>
+        </ItemWrapper>
     );
 }
